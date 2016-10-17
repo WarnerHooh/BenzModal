8c179c3fd71d775e9b65cf5c45ec422814e57297
Cheng Chen <mrcloudland@icloud.com>
2016-10-17 17:09:04 +0800

    Made update color work

--- Assets/Scripts/UpdateColor.cs
+++ Assets/Scripts/UpdateColor.cs
@@ -6,20 +6,24 @@ public class UpdateColor : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		car = GetComponent<Transform> ();
-		Transform[] children = car.GetComponentsInChildren<Transform>();
-		print (children[0]);
-//		Renderer rend = GetComponent<Renderer>();
-//		foreach (Material ma in rend.materials) {
-//			if(ma.name.Contains("body_paint")) {
-//				ma.SetColor ("_Color", Color.red);
-//			}
-//		}
-//		rend.material.shader = Shader.Find("Specular");
-//		rend.material.SetColor("_SpecColor", Color.red);
+		UpdateChildColor (car, Color.red);
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 	
+	}
+
+	void UpdateChildColor (Transform parentTransform, Color newColor) {
+		foreach (Transform child in parentTransform) {
+			Renderer rend = child.gameObject.GetComponent<Renderer> ();
+			if (rend) {
+				foreach (Material ma in rend.materials) {
+					if (ma.name.Contains ("body_paint")) {
+						ma.SetColor ("_Color", newColor);
+					}
+				}
+			}
+			UpdateChildColor (child, newColor);
+		}
 	}
 }
